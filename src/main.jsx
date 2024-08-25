e0b90da5339be04364827148a8f43f5c4564dee7
Abhishek Gawande <abhishekgawande1667@gmail.com>
2024-08-25 16:14:50 +0530

    adding pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    const swUrl = '/service-worker.js';
-    navigator.serviceWorker.register(swUrl).then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }).catch((error) => {
-      console.log('Service Worker registration failed:', error);
-    });
-  });
-}
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
